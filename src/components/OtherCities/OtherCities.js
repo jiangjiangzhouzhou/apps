a183f9f6eb2d1c6935460928f01d5d346a06698c
jiangjiangzhouzhou <jiangjianglovezhou@gmail.com>
2022-04-27 21:18:16 +1000

    feat:"get data"

--- src/components/OtherCities/OtherCities.js
+++ src/components/OtherCities/OtherCities.js
@@ -1,48 +1,30 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import styles from './OtherCities.module.css';
 import classNames from 'classnames/bind';
 import City from './components/City';
 
 const cx = classNames.bind(styles);
 
-export const CITIES = [{
-  name: 'Melbourne',
-  id: '2158177',
-  temp: "9",
-  weather: {icon: '11d', description: 'Thunderstorm' }
-}, {
-  name: 'Sydney',
-  id: '2147714',
-  temp: "15",
-  weather: { icon: '01d', description: 'Clear' } 
-}, {
-  name: 'Brisbane',
-  id: '2174003',
-  temp: "11",
-  weather: { icon: '03d', description: 'Clouds' }
-}, {
-  name: 'Perth',
-  id: '2063523',
-  temp: "7",
-  weather: { icon: '50d', description: 'Mist' }
-}];
-
-const OtherCities = () => {
+const OtherCities = ({currentCity, setCity, weathers}) => {
     return (
       <div className={styles.otherCities}>
         <div className={cx('container')}>
           <h2 className={styles.header}>Other Cities</h2>
           <div className={styles.cities}>
-            {CITIES.map(city => {
+            {weathers && weathers.map(city => {
+              if (city.name === currentCity.name) {
+                return null;
+              }
               return (
                 <City 
                   key={city.id}
                   name={city.name} 
-                  temperature={parseInt(city.temp)}
+                  temperature={parseInt(city.main.temp)}
                   weather={{ 
-                    icon: city.weather.icon, 
-                    description: city.weather.main,
-                  }} 
+                    icon: city.weather[0].icon, 
+                    description: city.weather[0].main,
+                  }}
+                  onClick = {() => setCity(city)}
                 />
               )
             })}
